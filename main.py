ace4d55643c990d627e39235bebd219cf62dc658
qw61116111 <75726728+qw61116111@users.noreply.github.com>
2021-06-21 18:54:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,12 +19,11 @@ shop_list=[0,1,8,9,11,13,17,20,23,27,29,30,32,33,40,43,51,54]
 def test(shop):
     data=count_month[22:34,shop]
     return data
-
+model=torch.load('C://Users/Q56091087/.spyder-py3/Class_AI/Final_hw/save.pt', map_location=torch.device('cpu'))
+model.eval()
 for shop in range(60):
     if shop not in shop_list:
-        print(shop)
-        model=torch.load('C://Users/Q56091087/.spyder-py3/Class_AI/Final_hw/%dsave.pt'%shop, map_location=torch.device('cpu'))
-        model.eval()
+
         test_data=test(shop).float()
         test_data=torch.unsqueeze(test_data,0)
         out=model(test_data)
@@ -32,7 +31,7 @@ for shop in range(60):
         out[torch.where(out>20)]=20
         temp[shop]=out.detach().numpy()
 
-with open('submission.csv', 'w', newline='') as csvfile:
+with open('C://Users/Q56091087/.spyder-py3/Class_AI/Final_hw/submission.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     for i in range(len(submit)):
         if i!=0:
